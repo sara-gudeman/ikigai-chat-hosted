7b735774cf46508ae8d15f93090900d2432a9732
Sara Gudeman <sgudeman982@gmail.com>
2025-08-07 21:08:55 -0400

    added note

--- client/src/components/VideoPlayer/VideoPlayer.tsx
+++ client/src/components/VideoPlayer/VideoPlayer.tsx
@@ -3,6 +3,7 @@ import './VideoPlayer.css';
 
 export const VideoPlayer = () => {
   // TODO: this is massive and should be optimized :)
+  // also this could be in assets
   return (
     <video autoPlay muted width="640" height="360">
       <source src={myVideo} type="video/mp4" />
